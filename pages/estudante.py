66eadc9eb5e2082aadec16f04bd42f258e5fb0e4
Pedro Farias <pfarias29@gmail.com>
2025-12-18 22:07:38 -0300

    feat: seleção de arquivo para estudante

--- pages/estudante.py
+++ pages/estudante.py
@@ -2,56 +2,8 @@ import streamlit as st
 import pdfplumber
 import re
 import pandas as pd
-
-padrao_normal = re.compile(
-    r"(?P<ano>\d{4}\.\d|--)\s+"
-    r"(?P<flag>[\*\#\&\@\§]?)\s*"
-    r"(?P<codigo>[A-Z]{3}\d{4})\s+"
-    r"(?P<ch>\d+)\s+"
-    r"(?P<turma>[A-Z0-9]{1,2}|--)\s+"
-    r"(?P<freq>\d{2,3},\d|--)\s+"
-    r"(?P<nota>[A-Z]{2}|-)\s+"
-    r"(?P<situacao>[A-Z]{3,4})"
-)
-
-padrao_com_nome = re.compile(
-    r"(?P<ano>\d{4}\.\d|--)\s+"
-    r"(?P<flag>[\*\#\&\@\§]?)\s*"
-    r"(?P<codigo>[A-Z]{3}\d{4})\s+"
-    r"(?P<nome>.+?)\s+"
-    r"(?P<ch>\d+)\s+"
-    r"(?P<turma>[A-Z0-9]{1,2}|--)\s+"
-    r"(?P<freq>\d{2,3},\d|--)\s+"
-    r"(?P<nota>[A-Z]{2}|-)\s+"
-    r"(?P<situacao>[A-Z]{3,4})"
-)
-
-padrao_nome_antes = re.compile(
-    r"(?P<ano>\d{4}\.\d)\s+"
-    r"(?P<codigo>[A-Z]{3}\d{4})\s+"
-    r"(?P<nome>.+?)\s+"
-    r"(?P<ch>\d+)\s+"
-    r"(?P<turma>[A-Z0-9]{1,2})\s+"
-    r"(?P<freq>\d{2,3},\d)\s+"
-    r"(?P<nota>[A-Z]{2})\s+"
-    r"(?P<situacao>[A-Z]{3})"
-)
-
-padrao_enade = re.compile(
-    r"(?P<ano>\d{4}\.\d)\s+"
-    r"(?P<codigo>ENADE)\s+"
-    r"(?P<ch>\d+)\s+--\s+--\s+---\s+--"
-)
-
-padroes = [
-    padrao_normal,
-    padrao_com_nome,
-    padrao_nome_antes,
-    padrao_enade
-]
-
-
-
+from io import StringIO
+import utils.padroes as padroes
 
 st.page_link("app.py", label="Voltar", icon=":material/keyboard_return:")
 
@@ -60,11 +12,12 @@ st.title("Área do estudante")
 st.write("##### Aqui você poderá inserir seu histórico para conseguir uma previsão de evasão.",)
 st.write("Baixe um exemplo do arquivo de entrada ou insira seu histórico para analisarmos.")
 
+uploaded_file = st.file_uploader("Selecionar histórico (PDF)", type="pdf")
 
-def read_file_pdf(path = "historico_211055577.pdf"):
+def read_file_pdf(arq):
     text = ""
 
-    with pdfplumber.open(path) as pdf:
+    with pdfplumber.open(arq) as pdf:
         for page in pdf.pages:
             text += page.extract_text() + "\n"
 
@@ -75,7 +28,7 @@ def read_file_pdf(path = "historico_211055577.pdf"):
     dados = []
 
     for linha in linhas:
-        for p in padroes:
+        for p in padroes.padroes:
             m = p.search(linha)
             if m:
                 dados.append({
@@ -96,4 +49,5 @@ def read_file_pdf(path = "historico_211055577.pdf"):
 
     print(df) 
 
-read_file_pdf()
+if uploaded_file:
+    read_file_pdf(uploaded_file)
---
+++ utils/padroes.py
@@ -0,0 +1,48 @@
+import re
+
+padrao_normal = re.compile(
+    r"(?P<ano>\d{4}\.\d|--)\s+"
+    r"(?P<flag>[\*\#\&\@\§]?)\s*"
+    r"(?P<codigo>[A-Z]{3}\d{4})\s+"
+    r"(?P<ch>\d+)\s+"
+    r"(?P<turma>[A-Z0-9]{1,2}|--)\s+"
+    r"(?P<freq>\d{2,3},\d|--)\s+"
+    r"(?P<nota>[A-Z]{2}|-)\s+"
+    r"(?P<situacao>[A-Z]{3,4})"
+)
+
+padrao_com_nome = re.compile(
+    r"(?P<ano>\d{4}\.\d|--)\s+"
+    r"(?P<flag>[\*\#\&\@\§]?)\s*"
+    r"(?P<codigo>[A-Z]{3}\d{4})\s+"
+    r"(?P<nome>.+?)\s+"
+    r"(?P<ch>\d+)\s+"
+    r"(?P<turma>[A-Z0-9]{1,2}|--)\s+"
+    r"(?P<freq>\d{2,3},\d|--)\s+"
+    r"(?P<nota>[A-Z]{2}|-)\s+"
+    r"(?P<situacao>[A-Z]{3,4})"
+)
+
+padrao_nome_antes = re.compile(
+    r"(?P<ano>\d{4}\.\d)\s+"
+    r"(?P<codigo>[A-Z]{3}\d{4})\s+"
+    r"(?P<nome>.+?)\s+"
+    r"(?P<ch>\d+)\s+"
+    r"(?P<turma>[A-Z0-9]{1,2})\s+"
+    r"(?P<freq>\d{2,3},\d)\s+"
+    r"(?P<nota>[A-Z]{2})\s+"
+    r"(?P<situacao>[A-Z]{3})"
+)
+
+padrao_enade = re.compile(
+    r"(?P<ano>\d{4}\.\d)\s+"
+    r"(?P<codigo>ENADE)\s+"
+    r"(?P<ch>\d+)\s+--\s+--\s+---\s+--"
+)
+
+padroes = [
+    padrao_normal,
+    padrao_com_nome,
+    padrao_nome_antes,
+    padrao_enade
+]
